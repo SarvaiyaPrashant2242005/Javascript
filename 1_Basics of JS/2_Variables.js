4367c5bdd766568e32926441af7caca23ccaaad6
SarvaiyaPrashant2242005 <115221462+SarvaiyaPrashant2242005@users.noreply.github.com>
2025-05-05 02:24:37 +0530

    Variable declaration and rules of declaration

---
+++ 1_Basics of JS/2_Variables.js
@@ -0,0 +1,52 @@
+// Variables can used to store the value or information to use them leteron
+// We can change that information letter
+// Variables are casesensitive
+// "use strict";
+
+// Declaration of Variable
+
+console.log("Variables")
+
+var firstName = "Prashant";
+var lastName = "Sarvaiya";
+
+
+// Use of Variable
+ console.log(firstName);
+ console.log(lastName);
+
+// Change The Value
+firstName = "Mahendra Singh";
+lastName = "Dhoni";
+console.log(firstName);
+console.log(lastName);
+
+
+// We can create in JS in without var but it creates problem letter on let's see
+
+cricketor = "Virat Kohli"
+console.log(cricketor);
+
+cricketer= "Hardik pandya"
+console.log(cricketor)
+
+// As you can see above in updation of value of variable even if 1 letter is changed then it creates the new variable inspite of updation which gives problem letteron
+
+// To overcome this we use useStrict at top which stop us to make mistakes remove comment to check
+
+
+// Rules of naming the Variables
+// variable name cannot be start with the number  : 1value, 
+// We can use number atlast  : value1
+var value1 = 10
+
+// Can be perform arithmatic operation directly
+console.log(value1 * 8);
+console.log(value1 ** 2)  //Get power of any value
+
+
+
+// We can use only _ or $ symboll in between the variable : !@#%^&*()
+// Space cannot be use in variables like : first name, last name,
+// camelCase Writing prefereed : firstName, goodBoy
+console.log("-----------------------------------------------")
