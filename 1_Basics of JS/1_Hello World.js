538cffbe860fbc083e269cd70380b8df98ec1ed3
SarvaiyaPrashant2242005 <115221462+SarvaiyaPrashant2242005@users.noreply.github.com>
2025-05-05 02:24:10 +0530

    First Hello World Programme

---
+++ 1_Basics of JS/1_Hello World.js
@@ -0,0 +1,9 @@
+
+//Comments are wriiten by double slash in js
+//Console.log print anything console
+//Can use single and double quote as well
+//Semicolumn is optional 
+
+console.log("HelloWorld");
+console.log("Prashant Sarvaiya");
+console.log("-----------------------------------------------")
