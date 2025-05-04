c0b4544eab7e693a07e75ddde565b11e9de8a79d
SarvaiyaPrashant2242005 <115221462+SarvaiyaPrashant2242005@users.noreply.github.com>
2025-05-05 02:24:55 +0530

    Let Keyword in JS

---
+++ 1_Basics of JS/3_let variables.js
@@ -0,0 +1,9 @@
+// console.log("Let variables")
+
+
+// Variable can be declare using let keyword
+
+let firstName = "Prashant";
+console.log(firstName)
+
+// let keyword not allowed to declare the same variable twise or again 
