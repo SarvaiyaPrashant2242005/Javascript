7b6d0cc7c7f33ce3e00d1b520f89e1642fbc6eef
prashant sarvaiya <prashanthere90@gmail.com>
2025-05-28 11:02:53 +0530

    boolean

---
+++ 1_Basics of JS/9_BoolandComp.js
@@ -0,0 +1,14 @@
+// Bollien 
+
+// Either true or falso
+
+let num1 = 59;
+let num2 = 90; 
+
+console.log(num1>num2);
+console.log(num1<num2);
+console.log(num1>=num2);
+console.log(num1<=num2);
+console.log(num1==num2); //Check if both the numbers are equals or not
+console.log(num1===num2); //Check if the datatype of the numbers are true
+console.log(num1!=num2); //check if the numbers are different 
