5a811ce5f3aebd5c0fa1b93f69fb0a510162bac0
prashant sarvaiya <prashanthere90@gmail.com>
2025-05-28 11:07:57 +0530

    Ternary operators

---
+++ 1_Basics of JS/12_Ternary.js
@@ -0,0 +1,14 @@
+let age  = 19
+let drink;
+
+// if(age >= 5){
+//    drink = "coffee"
+// }
+// else{
+//     drink = "milk"
+// }
+// console.log(drink);
+
+
+drink = age >= 5 ? "Coffee" : "MILK";
+console.log(drink);
