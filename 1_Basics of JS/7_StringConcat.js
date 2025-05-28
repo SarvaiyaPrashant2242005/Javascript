0a0b7cee62fa8d6bfb49b3f3190a41827c787b3e
prashant sarvaiya <prashanthere90@gmail.com>
2025-05-28 10:27:00 +0530

    String Concat

---
+++ 1_Basics of JS/7_StringConcat.js
@@ -0,0 +1,21 @@
+// String concat
+
+let s1= "Prashant"
+let s2 = "Sarvaiya"
+
+let fullname = s1 + " " +s2;
+console.log(fullname);
+
+
+let num1 = "23"
+let num2 = "56"
+
+let sum = +num1 + +num2;   //Converting string into the numbers 
+console.log(sum);
+
+
+console.log("My name is " + fullname + " and my age is " + num1);
+
+let aboutMe =`my name is ${fullname} and my age is ${num2}`
+console.log(aboutMe);
+
