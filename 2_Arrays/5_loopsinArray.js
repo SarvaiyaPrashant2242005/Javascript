b553b25b614fc1aebabf0c5026873d6fee28acd7
prashant sarvaiya <prashanthere90@gmail.com>
2025-05-29 03:41:34 +0530

    Loops in Array

---
+++ 2_Arrays/5_loopsinArray.js
@@ -0,0 +1,23 @@
+//For Loops
+
+let fruits = ["Mango", "Apple", "Banana", "Chikoo"];
+
+console.log(fruits.length)
+
+// Last index = length - 1
+let fruits1 = []
+for(let i=0;i<fruits.length;i++){
+    // console.log(fruits[i].toUpperCase());
+    fruits1.push(fruits[i].toUpperCase());
+}
+
+console.log(fruits);
+console.log(fruits1);
+
+
+
+// For of loop
+
+for(let fruit of fruits){
+    console.log(fruit.toUpperCase());
+}
