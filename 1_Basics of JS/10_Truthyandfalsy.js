5bd51428fdb6a907b828034ebc97cb5cf5b3dd1f
prashant sarvaiya <prashanthere90@gmail.com>
2025-05-28 11:03:13 +0530

    truthy and falsey values

---
+++ 1_Basics of JS/10_Truthyandfalsy.js
@@ -0,0 +1,10 @@
+let fn = "bj";
+// falsey : These values are interpreted as false when encountered in a boolean context:
+// Truthy : Any value that is not falsy is considered truthy. This includes:
+
+if(fn){
+    console.log(fn);
+}
+else{
+    console.log("FIRST kinda empty");
+}
