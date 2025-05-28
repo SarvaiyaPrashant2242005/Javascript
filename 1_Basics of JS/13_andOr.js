eb216fe4c3a5ee7a35aa3a42db8857df38a804c5
prashant sarvaiya <prashanthere90@gmail.com>
2025-05-28 12:12:26 +0530

    && and || operators

---
+++ 1_Basics of JS/13_andOr.js
@@ -0,0 +1,16 @@
+// And and or operators
+
+// And use to satisfies two or more conditions at a time
+
+let fn = "Prashant"
+let age = 21
+
+if(fn[0]=== "P" && age > 18){
+    console.log("Name starts with p and above 18");
+}
+
+// OR use to satiesfirs atleast one condition
+
+if(fn[0]==="P" || age>18){
+    console.log("Ho gya kam");
+}
