922fd3bf6230d6ee9b72318a9cad757c7a29e54a
prashant sarvaiya <prashanthere90@gmail.com>
2025-05-28 10:20:18 +0530

    typeOf operator and datatype conversion

---
+++ 1_Basics of JS/6_Operators.js
@@ -0,0 +1,23 @@
+// Typesof Operators 
+
+// datatypes
+    // String, integer, number, booliens, undefined, null, bigInt, Symbol
+
+// typeof shows the datatype of the variable
+
+let age = 22;
+let fn = "Prashant";
+
+// console.log(typeof(age))
+// console.log(typeof(fn))
+
+// Coverting the number into the string
+
+console.log(typeof (age + "") );
+console.log(typeof(String(age)));
+
+// Converting string into the number
+
+console.log(typeof(+fn));
+console.log(typeof(Number(fn)));
+
