85a8fe50c79d3e0eed030e3d39d72a132f87ae33
prashant sarvaiya <prashanthere90@gmail.com>
2025-05-28 12:12:59 +0530

    Loops in js

---
+++ 1_Basics of JS/15_Loops.js
@@ -0,0 +1,20 @@
+// 0 to 9 try to repeat yourself
+
+// while loop excutes atleast once
+let i=0
+
+// while (i<=10){
+//     console.log(i);
+//     i++;
+// }
+
+// // For Loop
+// for(let j=0;j<=10;j++){
+//     console.log(j);
+// }
+
+// Do while loop
+// do{
+//     console.log(i);
+//     i++
+// }while(i<=10)
