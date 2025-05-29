39b91f1dcaae313f6ea3af510872d8b4b190597e
prashant sarvaiya <prashanthere90@gmail.com>
2025-05-29 06:33:24 +0530

    Function Expression

---
+++ 4_Functions/2_Expression.js
@@ -0,0 +1,26 @@
+// Assign function into single variable is called function expression
+
+// Function is a block of code which design to do a particular task
+
+const sayHello = function(){
+    console.log("Hello World!!!!");
+}
+
+sayHello();
+
+
+const sum = function(num1, num2){
+    return num1 + num2;
+}
+
+console.log(sum(4,90));
+console.log(sum());
+
+
+const sum2 = function(num1,num2,num3){
+    return num1+num2+num3;
+}
+
+console.log(sum2(4,5,9));
+
+
