f76f2680b470dc1e31a47a38923af22a08531b95
prashant sarvaiya <prashanthere90@gmail.com>
2025-06-02 06:27:34 +0530

    sync and async in js

---
+++ 8_Asynchronous JavaScript/1_sync-async.js
@@ -0,0 +1,21 @@
+console.log("Script Started");              
+
+for (let i=0;i<1000;i++){
+    console.log("Inside the loop ");
+}
+
+console.log("Script Ended");
+
+
+// All of these will excexute simentenously it is call synchrounus javaScript
+
+
+// For loop is blocking further code in it is call synchrounus programming so we have started asynchrounus programming
+
+
+// JavaScript is a Synchronous Programming and single threaded.
+
+
+
+// How to do asynchronous Programming in JS
+
