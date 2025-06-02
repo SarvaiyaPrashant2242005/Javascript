63c2ac73317ce67a0ef51f258f6a7374b5a15f45
prashant sarvaiya <prashanthere90@gmail.com>
2025-06-02 06:28:04 +0530

    setInterval in js

---
+++ 8_Asynchronous JavaScript/3_setInterval.js
@@ -0,0 +1,11 @@
+console.log("Script Begins...");
+
+setInterval(() => {
+    console.log(Math.random());
+}, 1000);
+
+console.log("SCript Ended!!");
+
+
+
+// sets an interval while the code will excecute after every interval given in the function
